492533ead6a4a95db763f6c4a8f0021e62e99a56
quandq <quandq@biplus.com.vn>
2022-01-09 22:37:03 +0700

    update source

--- lib/scenes/login.dart
+++ lib/scenes/login.dart
@@ -1,5 +1,6 @@
 import 'package:exercise_example/base/screen_base.dart';
 import 'package:exercise_example/scenes/home.dart';
+import 'package:exercise_example/widgets/product_item.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -16,48 +17,57 @@ class _LoginScreen extends WidgetBase<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login Screen')),
-      body: Center(
-        child: Card(
-          margin: const EdgeInsets.all(16),
-          elevation: 6,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          child: Container(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: const Text('Login social', style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold)),
-                ),
-                Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Enter your username'
+      body: Stack(
+        children: [
+          Image.network(
+              'https://i.pinimg.com/736x/fb/b7/1d/fbb71d48275c528dac30741f0e665f56.jpg',
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              fit: BoxFit.fill),
+          Center(
+            child: Card(
+              margin: const EdgeInsets.all(16),
+              elevation: 6,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              child: Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: const Text('Login social', style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold)),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Enter your username'
+                        ),
                       ),
                     ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 3),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Enter your password'
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 3),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Enter your password'
+                        ),
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 0),
+                      child: OutlinedButton(onPressed: () {
+                        pushScreen(HomeScreen());
+                      }, child: Text('Submit')),
+                    )
+                  ],
                 ),
-                Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 0),
-                    child: OutlinedButton(onPressed: () {
-                      pushScreen(HomeScreen());
-                    }, child: Text('Submit')),
-                )
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        ],
+      )
     );
   }
 }
